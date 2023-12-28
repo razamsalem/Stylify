080f86e184d59667d645a398347b091a6e6e28db
Raz <amsalemraz10@gmail.com>
2023-12-28 11:18:32 +0200

    A toggle button to display the icon on the shirt has been implemented

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -47,7 +47,7 @@ const Customizer = () => {
     state[DecalType.stateProperty] = res
 
     if (!activeFilterTab[DecalType.filterTab]) {
-      handleActiveFilterTab(decalType.filterTab)
+      handleActiveFilterTab(DecalType.filterTab)
     }
   }
 
@@ -62,6 +62,13 @@ const Customizer = () => {
         state.isLogoTexture = true
         state.isFullTexture = false
     }
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
   }
 
   const readFile = (type) => {
@@ -117,8 +124,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => { }}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => { handleActiveFilterTab(tab.name) }}
               />
             ))}
           </motion.div>
